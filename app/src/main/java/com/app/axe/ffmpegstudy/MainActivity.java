8ca1e0a384a5f8395f5584c3fe31d93ea4f2109c
axeChen <1137366723@qq.com>
2018-08-29 12:32:33 +0800

    项目导入ffmpeg动态库
    Signed-off-by: axeChen <1137366723@qq.com>

---
+++ app/src/main/java/com/app/axe/ffmpegstudy/MainActivity.java
@@ -0,0 +1,37 @@
+package com.app.axe.ffmpegstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("avcodec-56");
+        System.loadLibrary("avdevice-56");
+        System.loadLibrary("avfilter-5");
+        System.loadLibrary("avformat-56");
+        System.loadLibrary("avutil-54");
+        System.loadLibrary("postproc-53");
+        System.loadLibrary("swresample-1");
+        System.loadLibrary("swscale-3");
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
